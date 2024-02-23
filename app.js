e18bff5f70256ae015a50ef6f796771ade35baef
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-23 21:16:05 +0500

    completed

---
+++ app.js
@@ -0,0 +1,61 @@
+/*--------------- MATH OBJECTS---------------*/
+
+
+// const area = 7.4;
+// console.log(Math.round(area))
+
+// console.log(Math.floor(area));
+
+// console.log(Math.ceil(area))
+
+// console.log(Math.trunc(area))
+
+// const randomNumber = Math.trunc(Math.random() * 10) + 1;
+
+// console.log(randomNumber);
+
+
+
+/*---------ko'p ishlatilinadigan mathodlar ---------- */
+// let email = "davronbekanvarov23@gmail.com";
+// LastIndexof()
+// let result = email.lastIndexOf("a");
+// console.log(result);
+
+// slice()
+
+// let result =email.slice(4,7)
+// console.log(result)
+
+//substr()
+
+// let result =email.substr(4,7)
+// console.log(result)
+
+// replace()
+
+// let result =email.replace('a','e')
+// console.log(result)
+
+// charAt()
+
+// let result = email.charAt();
+// console.log(result);
+
+// toUpperCase() - toLowerCase()
+
+// let result = email.toLocaleUpperCase();
+// console.log(result);
+
+// let result = email.toLocaleLowerCase();
+// console.log(result);
+
+// trim()
+
+// let result =email.trim()
+// console.log(result)
+
+//split()
+
+// let result = email.split('');
+// console.log(result);
